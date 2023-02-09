8f92ec35f0f96f92d06616306c6e3a02d1f13f30
Marllon Silibri <marllonmunhoz321@hotmail.com>
2023-02-09 23:56:18 +0000

    test

---
+++ app/src/main/java/com/example/configgit/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.configgit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //ksjfbsd´kv
+    }
+}
